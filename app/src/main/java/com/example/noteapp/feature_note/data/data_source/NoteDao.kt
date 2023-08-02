fd1c8cb0c3f033e4854b45a613ff637ecc42e66b
Lê Bùi Tấn Trưởng <lebuitantruong@gmail.com>
2023-08-02 17:39:17 +0700

    create

---
+++ app/src/main/java/com/example/noteapp/feature_note/data/data_source/NoteDao.kt
@@ -0,0 +1,24 @@
+package com.example.noteapp.feature_note.data.data_source
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.noteapp.feature_note.domain.model.Note
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoteDao{
+    @Query("Select * from note")
+    fun getNotes(): Flow<List<Note>>
+
+    @Query("Select * from note where id = :id")
+    suspend fun getNoteById(id: Int): Note?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNote(note: Note)
+
+    @Delete
+    suspend fun deleteNode(note: Note)
+}
---
+++ app/src/main/java/com/example/noteapp/feature_note/domain/use_case/NoteUserCases.kt
@@ -0,0 +1,3 @@
+package com.example.noteapp.feature_note.domain.use_case
+
+data class NoteUserCases(val getNotes: GetNotes, val deleteNote: DeleteNote)
